3db1a59e3a68e5367d477b4e0e6c6d4723a082ff
直人 中山 <nn1460@hotmail.co.jp>
2020-03-31 11:09:03 +0900

    [feat] Fix Drop Items Doesnt Sorted

--- PDFConcat/MainWindow.xaml.cs
+++ PDFConcat/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
@@ -55,6 +44,7 @@ namespace PDFConcat
         public void PDF_Drop(object sender, DragEventArgs e)
         {
             string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            Array.Sort(files);
             if (files == null)
             {
                 return;
